da79a8cbfd880098b0619d6f088497cbdad5c3ba
danieldiasm <daniel_diasm@hotmail.com>
2023-03-26 13:29:17 +0200

    small fixes

--- base_example.py
+++ base_example.py
@@ -1,16 +1,16 @@
 from rpi_lcd import LCD
-import signal
+from signal import signal
 
 lcd = LCD()
 def safe_exit(signum, frame):
     exit(1)
 try:
-    signal(SIGTERM, safe_exit)
-    signal(SIGHUP, safe_exit)
+    signal.signal(signal.SIGTERM, safe_exit)
+    signal.signal(signal.SIGHUP, safe_exit)
     lcd.text("First Line", 1)
     lcd.text("Second Line", 2)
     print("Press Ctrl+C to exit!")
-    pause()
+    input()
 except KeyboardInterrupt:
     pass
 finally:
