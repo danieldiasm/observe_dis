f095ac5758453aa70c7d4c87ab98debef7435f26
danieldiasm <daniel_diasm@hotmail.com>
2023-04-16 14:50:05 +0200

    display startup routine

---
+++ startup_routine/startup_display.py
@@ -0,0 +1,55 @@
+from rpi_lcd import LCD
+import time
+
+import post
+
+# Instances
+lcd = LCD()
+
+# CONSTANTS
+NETWORK_INTERFACE='wlan0'
+IP_ADDR = "0.0.0.0"
+SERVER_ADDR="0.0.0.0"
+SRV_UP=""
+JENKINS_UP=""
+
+
+def show_and_clear(line1, line2, delay):
+    lcd.text(line1, 1)
+    lcd.text(line2, 2)
+    time.sleep(delay)
+    lcd.clear()
+
+def show_only(line1, line2):
+    lcd.clear()
+    lcd.text(line1, 1)
+    lcd.text(line2, 2)
+
+def run_post():
+    global IP_ADDR, SRV_UP, JENKINS_UP
+    # Check Own IP
+    IP_ADDR = post.get_own_ip(NETWORK_INTERFACE)
+    print(IP_ADDR)
+
+    # Check if LCD is present
+    _ = post.check_i2c()
+
+    # Check if Build Server is online
+    SRV_UP = post.check_server(SERVER_ADDR)
+
+    # Check if Jenkins Service is Available
+    JENKINS_UP = post.check_jenkins(SERVER_ADDR)
+
+lcd.backlight(True)
+show_only("    P.O.S.T.    ", " Please Wait... ")
+time.sleep(0.5)
+
+run_post()
+DISPLAY_TIME=2
+show_and_clear(" Welcome ", " DevOps Display ", DISPLAY_TIME)
+show_and_clear("IP Addr wlan0 ", f"{IP_ADDR} ", DISPLAY_TIME)
+show_and_clear("Build Server", f"{SERVER_ADDR} ", DISPLAY_TIME)
+show_and_clear("Build Server", f"{SRV_UP} ", DISPLAY_TIME)
+show_and_clear("Jenkins Service", f"{JENKINS_UP} ", DISPLAY_TIME)
+lcd.backlight(False)
+
